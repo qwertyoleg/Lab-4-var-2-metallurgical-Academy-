6994a3d961f1675ea12d6edbcdb7a55af74f0722
Oleg Petrushov <olegpetrushov@icloud.com>
2016-01-24 12:04:22 +0200

    initial commit

---
+++ Laboratory worck 4 var 2/main.cpp
@@ -0,0 +1,90 @@
+//
+//  main.cpp
+//  Laboratory worck 4 var 2
+//
+//  Created by Oleg Petrushov on 1/7/16.
+//  Copyright © 2016 XcoderUA. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class EndlessSuperStreing
+{
+private:
+    /* список свойств и методов для использования внутри класса */
+    char *myCharArray;
+public:
+    /* список методов доступных другим функциям и объектам программы */
+    EndlessSuperStreing()
+    {
+        myCharArray = new char[15];
+        myCharArray[0] = '\0';
+    }
+    
+    
+    
+    void reinitWithArray(char *aArray)
+    {
+        int arrayCounter = 0;
+        while(aArray[arrayCounter]!='\0')
+        {
+            arrayCounter++;
+        }
+        myCharArray= new char[arrayCounter];
+        for (int i=0; i<=arrayCounter; i++)
+        {
+            myCharArray[i] = aArray[i];
+        }
+    }
+    
+    void reinitWithString(string aString)
+    {
+        myCharArray = new char[aString.length() + 1];
+        strcpy(myCharArray, aString.c_str());
+    }
+    void print()
+    {
+        cout<<myCharArray<<endl;
+    }
+    
+    EndlessSuperStreing operator+(EndlessSuperStreing strObj);
+    void setString(char *aStr)
+    {
+        myCharArray = aStr;
+    }
+protected:
+    /*список средств, доступных при наследовании*/
+};
+////friend +  (конкатонация строк)
+
+/*
+EndlessSuperStreing EndlessSuperStreing::operator+(EndlessSuperStreing strObj)
+{
+    //this->myString
+    return temp;
+}
+ */
+////friend -  (конкатонация строк с удалением
+////	    пробельных символов в конце
+////	первой и начале второй строки)
+////friend +=
+////friend -=
+
+int main(int argc, const char * argv[])
+{
+    EndlessSuperStreing obj1 = *new EndlessSuperStreing;
+    obj1.reinitWithString("Test string");
+    
+    cout<<"Obj1:";
+    obj1.print();
+    
+    EndlessSuperStreing obj2 = *new EndlessSuperStreing;
+    char ch[]  = {'t','e','s','t','\0'};
+    obj2.reinitWithArray(ch );
+    cout<<"Obj2:";
+    obj2.print();
+    
+    return 0;
+}
